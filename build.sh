27e4ca65117f591c138e5a3638c8a3ccfb4db82d
Shams Parvez Arka <38325426+ShamsParvezArka@users.noreply.github.com>
2023-01-10 15:44:22 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -xe
 
-mkdir bin/
+mkdir -p bin/
 gcc -o bin/gol src/game_of_life_CLI.c -Wall -Wextra -Wformat -std=c11
 gcc -o bin/raygol src/game_of_life_GUI.c -Wall -Wextra -Wformat -std=c11 -lm -lraylib -lGL -lpthread -ldl -lrt -lX11
